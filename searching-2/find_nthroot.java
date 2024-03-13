fed8d54b71b19fc6f518c7249de2612ed39a067f
KHATIYAN19 <121935517+KHATIYAN19@users.noreply.github.com>
2024-03-13 21:20:50 +0530

    Update find_nthroot.java

--- searching-2/find_nthroot.java
+++ searching-2/find_nthroot.java
@@ -1,20 +1,19 @@
-class Solution
-{
-    public int NthRoot(int n, int m)
-    {
-        // code here
-        int s=1,e=m;
+class Solution{
+    public int NthRoot(int n, int m){
+        int s=1;
+        int e=m;
         int ans=-1;
-        while(s<=e)
-        {
+        while(s<=e) {
             int mid=(s+e)/2;
-            int cal=(int)Math.pow(mid,n);
-            if(cal==m)
-                ans=(int)mid;
-            if(cal<m)
-             s=mid+1;
-            else
-             e=mid-1;
+            int power=(int)Math.pow(mid,n);
+            if(cal==m){
+                 ans=mid;
+            } 
+            if(power<m){
+                 s=mid+1;
+            }else{
+                 e=mid-1;
+            }   
         }
         return ans;
     }
