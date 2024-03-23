641e9f03e8d0324ad6305d7c35844e6f8bae400e
KHATIYAN19 <121935517+KHATIYAN19@users.noreply.github.com>
2024-03-23 18:12:13 +0530

    Create nthtermdivisiblebyabc.java

---
+++ search-5/nthtermdivisiblebyabc.java
@@ -0,0 +1,31 @@
+class solution
+{
+    public static int gcd(int a, int b) { 
+        if (a == 0) 
+            return b;  
+        return gcd(b % a, a); 
+    } 
+    public static int lcm(int a, int b) { 
+        return (a * b) / gcd(a, b); 
+    } 
+    public static int divTermCount(int a, int b, int c, int num){ 
+        return ((num / a) + (num / b) + (num / c)-(num / lcm(a, b))-(num / lcm(b, c))-(num / lcm(a, c))+(num / lcm(a, lcm(b, c))));         
+     } 
+    public static int findNthTerm(int a, int b, int c, int n){ 
+        int low = 1;
+        int high = Integer.MAX_VALUE, mid; 
+        while (low < high) { 
+            mid = low + (high - low) / 2;  
+            if (divTermCount(a, b, c, mid) < n) 
+                low = mid + 1; 
+            else
+                high = mid; 
+        }
+        return low; 
+    }  
+    public static void main(String[] args) 
+    { 
+        int a = 2, b = 3, c = 5, n = 10; 
+        System.out.println(findNthTerm(a, b, c, n)); 
+    } 
+}
