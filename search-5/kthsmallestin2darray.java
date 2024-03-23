b2a67a272d07c11d7e76aee8d17bf083d55aee3a
KHATIYAN19 <121935517+KHATIYAN19@users.noreply.github.com>
2024-03-23 18:15:51 +0530

    Create kthsmallestin2darray.java

---
+++ search-5/kthsmallestin2darray.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution{
+public static int kthSmallest(int[][]mat,int n,int k){
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                pq.add(mat[i][j]);
+                   if(pq.size()>k) {
+                    pq.poll();
+                }
+            }
+        }  
+        return pq.peek();
+    }
